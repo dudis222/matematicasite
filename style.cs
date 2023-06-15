a694949013365c5d7568283a2022855e1ec9b36a
dudis222 <107858735+dudis222@users.noreply.github.com>
2023-06-15 11:48:20 -0300

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,72 @@
+root {
+    --cor-de-fundo: #ffffff;
+    }
+    body {
+        background-color: var(--cor-de-fundo);
+        }
+h1 {
+    background-color: rgb(216, 205, 205);
+    }
+    .cabecalho__menu-hamburguer {
+        width: 24px;
+        height: 24px;
+        background-image: url("../img/Menu.svg");
+        display: inline-block;
+        }
+        @import url("styles/header.css");
+        :root {
+            --cor-de-fundo: #EBECEE;
+            --branco: #FFFFFF;
+            }
+            .cabecalho {
+                background-color: var(--branco);
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                }
+                .container {
+                    display: flex;
+                    align-items: center;
+                    }
+    .lista-menu {
+    display: none;:
+    position: absolute;
+            top: 100%;
+            }
+        .container__botao:checked ~ .lista-menu {
+            display: block;
+            }
+            .cabecalho {
+                background-color: var(--branco);
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                position: relative;
+                }
+                <head>
+
+<meta charset=UTF-8>
+<meta name=viewport content=width=device-width, initial-scale=1.0>
+<title>AluraBooks</title>
+<link rel=stylesheet href=styles.css>
+<link rel=preconnect href=https://fonts.googleapis.com>
+<link rel=preconnect href=https://fonts.gstatic.com crossorigin>
+<link href=https://fonts.googleapis.com/
+
+css2?family=Poppins:wght@300;400;500;700&display=swap rel=stylesheet>
+
+<link rel=stylesheet href=reset.css>
+</head>
+:root {
+--cor-de-fundo: #EBECEE;
+--branco: #FFFFFF;
+--laranja: #EB9B00;
+--azul-degrade: linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%);
+--fonte-principal: Poppins;
+}
+body {
+background-color: var(--cor-de-fundo);
+font-family: var(--fonte-principal);
+font-size: 16px;
+font-weight: 400;
+}
